39fd7af2befb022894fc4e5d0503f7c1bcdacb5e
wolfpirker <w_pirker@gmx.de>
2021-12-29 22:18:32 +0100

    adding manual validations

--- src/formOne.js
+++ src/formOne.js
@@ -7,15 +7,37 @@ const FormOne = () => {
     return (
         <Formik
             initialValues={{
-                firstname: 'steve',
+                firstname: '',
                 lastname: '',
                 email: '',
                 country: '',
                 state: '',
                 zip: ''
             }}
-            onSubmit={() => {
-                console.log('form submitted')
+            validate={values => {
+                const errors = {};
+
+                if (!values.firstname) {
+                    errors.firstname = 'Sorry, this is required';
+                }
+
+                if (!values.lastname) {
+                    errors.lastname = 'Sorry, this is required';
+                }
+
+                if (!values.email) {
+                    errors.email = 'Sorry, this is required';
+                } else if (
+                    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(values.email)
+                ) {
+                    errors.email = 'Invalid email';
+                }
+
+
+                return errors;
+            }}
+            onSubmit={(values) => {
+                console.log(values)
             }}
         >
             {({
@@ -25,7 +47,6 @@ const FormOne = () => {
                 handleChange,
                 handleBlur,
                 handleSubmit
-                // isSubmitting, etc.
             }) => (
                 <div className="container">
                     <div className="col-md-12 mt-5">
@@ -42,24 +63,57 @@ const FormOne = () => {
                                         name="firstname"
                                         value={values.firstname}
                                         onChange={handleChange}
+                                        onBlur={handleBlur}
                                     />
+                                    {errors.firstname && touched.firstname ?
+                                        <span>{errors.firstname}</span>
+                                        : null}
                                 </div>
                                 <div className="col-md-6 mb-3">
                                     <label htmlFor="lastname">Last name</label>
-                                    <input type="text" className="form-control" id="lastname" name="lastname" />
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="lastname"
+                                        name="lastname"
+                                        value={values.lastname}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                    {errors.lastname && touched.lastname ?
+                                        <span>{errors.lastname}</span>
+                                        : null}
                                 </div>
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" id="email" name="email" placeholder="you@example.com" />
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    name="email"
+                                    placeholder="you@example.com"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                {errors.email && touched.email ?
+                                    <span>{errors.email}</span>
+                                    : null}
                             </div>
 
 
                             <div className="row">
                                 <div className="col-md-5 mb-3">
                                     <label htmlFor="country">Country</label>
-                                    <select className="custom-select d-block w-100" id="country" name="country">
+                                    <select
+                                        className="custom-select d-block w-100"
+                                        id="country"
+                                        name="country"
+                                        value={values.country}
+                                        onChange={handleChange}
+                                    >
                                         <option value="">Choose...</option>
                                         <option value="US">United States</option>
                                         <option value="CA">Canada</option>
@@ -69,7 +123,13 @@ const FormOne = () => {
                                 </div>
                                 <div className="col-md-4 mb-3">
                                     <label htmlFor="state">State</label>
-                                    <select className="custom-select d-block w-100" id="state" name="state">
+                                    <select
+                                        className="custom-select d-block w-100"
+                                        id="state"
+                                        name="state"
+                                        value={values.state}
+                                        onChange={handleChange}
+                                    >
                                         <option value="">Choose...</option>
                                         <option value="california">California</option>
                                         <option value="toronto">Toronto</option>
@@ -79,7 +139,14 @@ const FormOne = () => {
                                 </div>
                                 <div className="col-md-3 mb-3">
                                     <label htmlFor="zip">Zip</label>
-                                    <input type="text" className="form-control" id="zip" name="zip" />
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="zip"
+                                        name="zip"
+                                        value={values.zip}
+                                        onChange={handleChange}
+                                    />
                                 </div>
                             </div>
 
