eebf99d18dff3ce14732769aa450a95519fa48a9
wolfpirker <w_pirker@gmx.de>
2022-01-01 22:26:23 +0100

    Formik Custom components in FormThree

--- src/formThree.js
+++ src/formThree.js
@@ -1,24 +1,59 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const FormThree = () => {
-    return (
-        <div className="container">
-            <div className="col-md-12 mt-5">
-                <form>
-                    <label htmlFor="firstname">First name</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="firstname"
-                    />
 
-                    <hr className="mb-4" />
-                    <button className="btn btn-primary btn-lg btn-block" type="submit">
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </div>
+    const formikProps = {
+        initialValues: { firstname: '', color: '' },
+        validationSchema: Yup.object({
+            firstname: Yup
+                .string()
+                .required('Sorry, this is required')
+        }),
+        onSubmit: values => {
+            console.log(values)
+        }
+    }
+
+
+    return (
+        <Formik {...formikProps}>
+            {formik => (
+                <div className="container">
+                    <div className="col-md-12 mt-5">
+                        <Form>
+                            <label htmlFor="firstname">First name</label>
+                            <Field name="firstname" type="text" className="form-control" />
+                            {formik.values.firstname}
+                            <ErrorMessage name="firstname" />
+                            {formik.errors.firstname && formik.touched.firstname ?
+                                <span>{formik.errors.firstname}</span>
+                                : null}
+                            <hr className="mb-4" />
+
+                            <label htmlFor="color">Color</label>
+                            <Field
+                                as="select"
+                                name="color"
+                                className="custom-select"
+                            >
+                                <option value="red">Red</option>
+                                <option value="green">Green</option>
+                                <option value="blue">Blue</option>
+                            </Field>
+
+                            <hr className="mb-4" />
+                            <button className="btn btn-primary btn-lg btn-block" type="submit">
+                                Submit
+                            </button>
+                        </Form>
+                    </div>
+                </div>
+
+            )}
+        </Formik>
+
 
     )
 }
