7af3821bc218cacec7c0f6110e046e88365b86f3
wolfpirker <w_pirker@gmx.de>
2022-01-01 21:13:48 +0100

    use formik hook

--- src/formtwo.js
+++ src/formtwo.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 
 const FormTwo = () => {
+    const formik = useFormik({
+        initialValues: { firstname: '' },
+        validationSchema: Yup.object({
+            firstname: Yup
+                .string()
+                .required('Sorry, this is required')
+        }),
+        onSubmit: values => {
+            console.log(values)
+        }
+    })
+
     return (
         <div className="container">
             <div className="col-md-12 mt-5">
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                     <label htmlFor="firstname">First name</label>
                     <input
                         className="form-control"
                         type="text"
                         name="firstname"
+                        {...formik.getFieldProps('firstname')}
+
+                    // onChange={formik.handleChange}
+                    // onBlur={formik.handleBlur}
+                    // value={formik.values.firstname}
                     />
+                    {formik.errors.firstname && formik.touched.firstname ?
+                        <span>{formik.errors.firstname}</span>
+                        : null}
 
                     <hr className="mb-4" />
                     <button className="btn btn-primary btn-lg btn-block" type="submit">
