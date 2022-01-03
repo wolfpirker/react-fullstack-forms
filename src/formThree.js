7eef784be9e0bd47b19ef3abc3580261e9e2ac5a
wolfpirker <w_pirker@gmx.de>
2022-01-03 21:53:21 +0100

    use Custom Fields in Formik, Forms 3

--- src/formThree.js
+++ src/formThree.js
@@ -5,16 +5,39 @@ import * as Yup from 'yup';
 const FormThree = () => {
 
     const formikProps = {
-        initialValues: { firstname: '', color: '' },
+        initialValues: { firstname: '', color: '', lastname: '', age: '' },
         validationSchema: Yup.object({
-            firstname: Yup
-                .string()
-                .required('Sorry, this is required')
+            firstname: Yup.string().required('Sorry, this is required'),
+            lastname: Yup.string().required('Sorry, this is required'),
+            age: Yup.string().required('Sorry, this is required')
         }),
         onSubmit: values => {
             console.log(values)
         }
     }
+
+    const formikInputCustomField = ({
+        field, // {name, value, onChange, onBlur}
+        form: { touched, errors },
+        ...props
+    }) => (
+        <>
+            <label htmlFor={field.name}>{props.labelName}</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder={props.placeholder}
+                {...field}
+            />
+            {errors[field.name] && touched[field.name] ?
+                <span>{errors[field.name]}</span>
+                : null
+            }
+        </>
+
+    )
+
+
 
 
     return (
@@ -32,7 +55,7 @@ const FormThree = () => {
                                 : null}
                             <hr className="mb-4" />
 
-                            <label htmlFor="color">Color</label>
+                            <label htmlFor="color">color selection</label>
                             <Field
                                 as="select"
                                 name="color"
@@ -42,6 +65,23 @@ const FormThree = () => {
                                 <option value="green">Green</option>
                                 <option value="blue">Blue</option>
                             </Field>
+
+                            <hr className="mb-4" />
+
+                            <Field
+                                name="lastname"
+                                component={formikInputCustomField}
+                                placeholder="Last name"
+                                labelName="Enter your lastname"
+                            />
+
+                            <hr className="mb-4" />
+                            <Field
+                                name="age"
+                                component={formikInputCustomField}
+                                placeholder="put your age"
+                                labelName="Enter your age"
+                            />
 
                             <hr className="mb-4" />
                             <button className="btn btn-primary btn-lg btn-block" type="submit">
